21bf5c0cdd6a42365b9623ab4d6c2245c59f64a3
OrhanMA <orhan.madi.assani@gmail.com>
2023-05-25 18:13:12 +0200

    made fixture using faker for User entity and made database url config in env

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,23 +5,27 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Provider\PhoneNumber;
+use DateTime;
+
 
 class UserFixture extends AbstractFixture
 {
     public function load(ObjectManager $manager): void
     {
+        $this->faker->addProvider(new PhoneNumber($this->faker));
+        $roles = ['Utilisateur', 'Administrateur'];
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
-            $user->setEmail($this->faker->word());
-            $user->setPassword();
-            $user->setRole();
-            $user->setFirstName();
-            $user->setLastName();
-            $user->setPhone();
-            $user->setCreated();
+            $user->setEmail($this->faker->email);
+            $user->setPassword($this->faker->password(8, 16));
+            $user->setRole($this->faker->randomElement($roles));
+            $user->setFirstName($this->faker->firstName);
+            $user->setLastName($this->faker->lastName);
+            $user->setPhone($this->faker->numberBetween(600000000, 699999999));
+            $user->setCreated(new DateTime);
+            // $user->setCreated();
             $manager->persist($user);
-            // $product = new Product();
-            // $manager->persist($product);
         }
 
         $manager->flush();
