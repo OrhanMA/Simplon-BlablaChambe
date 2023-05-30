f0adfdd682e6ddcdf431c0b2c34c23f5515bfc44
OrhanMA <orhan.madi.assani@gmail.com>
2023-05-30 15:46:37 +0200

    added rides list

---
+++ README.md
@@ -0,0 +1,2 @@
+Changer la bdd selon la plateforme dans le env
+Change le type array to string dans le UserInterface (fichier dans vendor)
--- src/Controller/TrajetsController.php
+++ src/Controller/TrajetsController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Ride;
+use App\Entity\User;
 
 class TrajetsController extends AbstractController
 {
     #[Route('/trajets', name: 'app_trajets')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // J'accède au repertoire de la classe Product grâce au EntityManagerInterface
+        $rideRepository = $entityManager->getRepository(Ride::class);
+        $userRepository = $entityManager->getRepository(User::class);
+        $allRides = $rideRepository->findAll();
+        $users = $userRepository->findAll();
         return $this->render('trajets/index.html.twig', [
             'controller_name' => 'TrajetsController',
+            'rides' => $allRides,
+            'users' => $users,
         ]);
     }
+
 }
